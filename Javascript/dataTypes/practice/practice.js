b67ff0d79e59219d740f675d4953d33c9630ae9f
yatoogamii <bracaval.elias.pro@gmail.com>
2020-02-27 21:49:46 +0100

    updated practice js dataTypes

---
+++ Javascript/dataTypes/practice/practice.js
@@ -0,0 +1,67 @@
+// Number
+
+let number = 12456;
+
+let addition = 1 + 2;
+let soustraction = 1 - 2;
+let multiplication = 1 * 2;
+let puissance = 1 ** 2;
+let modulo = 1 % 2;
+let incrementation = 1;
+incrementation++;
+let decrementation = 1;
+decrementation--;
+
+
+// Infinity
+let infinity = 1 / 0; // Infinity
+
+// NaN
+let nan = "not a number" / 2; // NaN
+
+
+// BigInt
+
+let bigInt = 12346518676516n;
+
+
+// String
+
+let singleQuote = 'Hello';
+let doubleQuote = "Hello";
+let backTicks = `Hello`;
+
+let example = `the result is: ${singleQuote}`; // the result is: Hello
+
+let firstName = "Elias";
+let lastName = "bracaval";
+
+let fullName = `${firstName} ${lastName}` // Elias bracaval
+
+
+// Boolean
+
+let isGreater = 4 > 1; // true
+
+
+// null
+let x = null;
+
+// undefined
+let x;
+
+
+// Object and Symbols
+
+
+
+// typeof
+
+let test = 15156;
+
+typeof test // number
+typeof(test) // number
+
+
+
+typeof null // object
